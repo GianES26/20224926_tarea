0151cf2900ddb5d83e3911ce14fc556cd1336b70
You <you@example.com>
2024-10-30 23:41:04 -0500

    Se ha sumado el proyecto en Java

---
+++ Gianfranco_Enriquez-20224926-Java/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Primera modificacion");
+        System.out.printf("Contaremos del 1 al 10");
+        System.out.printf("1");
+        System.out.printf("2");
+        System.out.printf("3");
+        System.out.printf("4");
+        System.out.printf("5");
+        System.out.printf("6");
+        System.out.printf("7");
+        System.out.printf("8");
+        System.out.printf("9");
+        System.out.printf("10");
+    }
+}
